32b650c8a4b39398b44e2983030ef149e6a73c66
hhhirokunnn <m2017hiro@gmail.com>
2020-05-19 14:31:07 +0900

    initial commit

---
+++ src/test/kotlin/hhhirokunn/sampleapi/SampleApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package hhhirokunn.sampleapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SampleApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
